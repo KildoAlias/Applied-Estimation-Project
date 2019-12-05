314ab59fe1763c9c8e24ea2b72fd0e9cdc909ae2
Oskar Erlandsson <oskar@n171-p183.eduroam.kth.se>
2019-12-05 16:19:22 +0100

    -

--- colorhistogram.m
+++ colorhistogram.m
@@ -1,8 +1,28 @@
  %Split into RGB Channels
-    image=imread('')
+    image1=imread("black_ball_white_back1.jpg");
+    scale_factor=1/4;
+    
+    image= imresize(image1, scale_factor);
     Red = image(:,:,1);
     Green = image(:,:,2);
     Blue = image(:,:,3);
+    
+    Red(Red>100)=0;
+    Green(Green>100)=0;
+     Blue(Blue>100)=0;
+    
+    Newimage(:,:,1)=Red;
+    Newimage(:,:,2)=Green;
+    Newimage(:,:,3)=Blue;
+    
+    figure()
+    imshow(Newimage)
+    
+   figure()
+   subplot(1,2,1)
+   imshow(image1)
+   subplot(1,2,2)
+   imshow(image)
     
     
     %Get histValues for each channel
