322fa248a6723fac2a48a707a241988f15526df0
KildoAlias <kildo.alias@live.com>
2019-12-08 12:49:22 +0100

    modified files

--- codes/histotest.m
+++ codes/histotest.m
@@ -1,59 +1,21 @@
-%close all
     image=imread('black_ball_white_back1.jpg');
-    Red = image(:,:,1);
-    Green = image(:,:,2);
-    Blue = image(:,:,3);
-   
-    %Get histValues for each channel
-    %[yRed, x] = imhist(Red);
-    %[yGreen, x] = imhist(Green);
-    %[yBlue, x] = imhist(Blue);
-  
+
+  % reference image
+    scale_factor=0.2;
     I=rgb2gray(image);
-    I=imresize(I,0.5);
-    thres=70;
-    idx=I>thres;
+    I=imresize(I,scale_factor);
+
     
-    sz=size(I);
-   % I=single(I);
-    mask=zeros(sz);
-    x=390;
-    y=165;
-    mask(x:x+200,y:y+200)=1;
-    mask=uint8(mask);
+    refimage=imread('black_ball_white_back4.jpg');
+    refimage=imresize(refimage,scale_factor);
     
-    masked_image=mask.*I;
-    masked_image_part=masked_image(x:x+200,y:y+200);
-    subplot(3,2,1)
-    imshow(masked_image_part)
+    I=I(1:300,:,:);
+    imshow(refimage)
     title('ref')
-    subplot(3,2,2)
-    imhist(masked_image_part);
-    hist1=imhist(masked_image_part);
-    
-    
-    
-    mask=zeros(sz);
-    x=390;
-    y=110;
-    mask(x:x+200,y:y+200)=1;
-    mask=uint8(mask);
-    
-    masked_image=mask.*I;
-    masked_image_part=masked_image(x:x+200,y:y+200);
-    subplot(3,2,3)
-    imshow(masked_image_part)
-    title('input hist')
-    hist2=imhist(masked_image_part);
-    subplot(3,2,4)
-    imhist(masked_image_part);
-    
-    [costvalue,costs]=histogram_matching(hist2,hist1,"none",[1,90]);
-    subplot(3,2,5)
-    plot(costs)
-    title(["costvalue= ",num2str(costvalue)])
-    
-    
+   
+    width=50;
+    [mini,costmap]=slow_sllide(I,refimage,width,width);
+    I=I(mini(1):mini(1)+width,mini(2):mini(2)+width);
     
     
      
